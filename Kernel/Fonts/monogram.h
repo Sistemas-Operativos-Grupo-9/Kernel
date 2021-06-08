d3972d7dd8e1afefee844daac7dd6e1d378a232e
Alan Sartorio <alan42ga@hotmail.com>
2021-06-08 19:08:50 -0300

    Font update

--- Kernel/Fonts/monogram.h
+++ Kernel/Fonts/monogram.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #define FONT_WIDTH 6
 #define FONT_HEIGHT 12
-#define FONT_BPP 1
+#define FONT_BPP 2
 typedef uint8_t FONT_ROW_TYPE;
 uint8_t font_letters[188][12] = {
     {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
