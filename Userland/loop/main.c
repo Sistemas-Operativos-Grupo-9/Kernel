1354d73e7137d7184026a97f29e605ad4bac0722
Alan Sartorio <alan42ga@hotmail.com>
2021-06-10 23:35:56 -0300

    Added loop program

---
+++ Userland/loop/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <print.h>
+#include <time.h>
+
+int main() {
+    char str[] = "loop";
+
+    for (int i = 0; i < 100; i++) {
+        putchar(str[i % 4]);
+        wait();
+    }
+    putchar('\n');
+
+    return 0;
+}
