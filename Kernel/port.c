f8677f6d92edc012ef954cfcd5dfcff89675c488
Alan Sartorio <alan42ga@hotmail.com>
2021-11-07 20:48:55 -0300

    Fixed kernel compilation with flto and optimizations.

--- Kernel/port.c
+++ Kernel/port.c
@@ -3,11 +3,11 @@
 uint8_t in(uint16_t port) {
 	uint8_t value;
 
-	__asm__("inb %w1, %0;" : "=a"(value) : "Nd"(port));
+	__asm__ __volatile__("inb %w1, %0;" : "=a"(value) : "Nd"(port));
 
 	return value;
 }
 
 void out(uint16_t port, uint8_t value) {
-	__asm__("outb %b1, %w0;" : : "Nd"(port), "a"(value));
+	__asm__ __volatile__("outb %b1, %w0;" : : "Nd"(port), "a"(value));
 }
