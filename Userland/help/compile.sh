7ea289aa5cb9f91fd6c69dbba7a5faefd90e94aa
Alan Sartorio <alan42ga@hotmail.com>
2021-11-09 21:46:40 -0300

    Added individual help for each program.

---
+++ Userland/help/compile.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+helpFiles=$(find ../* -name help.txt)
+
+exec 3>"compiled.txt"
+
+for helpFile in $helpFiles
+do
+    programName=$(sed -r 's|.*/(.*)/.*|\1|'<<<$helpFile)
+
+    echo "$programName: $(cat $helpFile)" >&3
+done
+
+exec 3>&-
+
+exec 3>"help.c"
+echo '#include "help.h"' >&3
+xxd -i compiled.txt >&3
+exec 3>&-
