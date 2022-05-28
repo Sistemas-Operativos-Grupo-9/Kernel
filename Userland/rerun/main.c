1bd085b429ca2101235aa63a73ab136d0aa41d90
Alan Sartorio <alan42ga@hotmail.com>
2022-05-28 20:34:26 -0300

    Fixed special case when rerun tries executing an unexistent program.

--- Userland/rerun/main.c
+++ Userland/rerun/main.c
@@ -1,3 +1,4 @@
+#include <shared-lib/print.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -8,11 +9,17 @@ int main(int argc, char **argv) {
 		char *module = argv[0];
 		char **args = argc > 0 ? &argv[1] : NULL;
 		int p;
-		switch(p = fork()) {
-			case 0:
-				exec(module, args);
-			default:
-				waitpid(p);
+		switch (p = fork()) {
+		case 0:
+			if (!exec(module, args)) {
+				puts("Module does not exist.");
+				return -1;
+			}
+		default: {
+			int res;
+			if ((res = waitpid(p)) != 0)
+				return res;
+		}
 		}
 	}
 	return 0;
