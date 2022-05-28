f387f71e4ae2543cfe65f896ab076a63f4189a19
Agustin <agalarza@itba.edu.ar>
2022-05-28 00:11:52 -0300

    added string functions

--- Userland/libc/string.c
+++ Userland/libc/string.c
@@ -14,6 +14,13 @@ void strcpy(char *dst, const char *src) {
 	} while (*(dst - 1) != '\0');
 }
 
+void strncpy(char *dst, const char *src, size_t n) {
+	// https://stackoverflow.com/questions/14159625/implementation-of-strncpy
+	size_t i = 0;
+	while (i++ != n && (*dst++ = *src++))
+		;
+}
+
 int strcmp(char *str1, char *str2) {
 	do {
 		if (*str1 > *str2)
@@ -22,6 +29,149 @@ int strcmp(char *str1, char *str2) {
 			return -1;
 	} while (*str1++ != '\0' && *str2++ != '\0');
 	return 0;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+	// from https://stackoverflow.com/questions/32560167/strncmp-implementation
+	while (n && *s1 && (*s1 == *s2)) {
+		++s1;
+		++s2;
+		--n;
+	}
+	if (n == 0)
+		return 0;
+	else
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+size_t tokencount(char *str, char c) {
+	size_t count = 0;
+	size_t i = 0;
+	while (str[i] != '\0') {
+		if (str[i] == c)
+			count++;
+		i++;
+	}
+	return i;
+}
+
+size_t deleteNCharsAtIndex(char *str, size_t index, size_t n) {
+	size_t moved = 0;
+	for (; moved < n && str[index + moved] != '\0'; moved++)
+		;
+	while (str[index + moved] != '\0') {
+		str[index] = str[index + moved];
+		index++;
+	}
+	str[index] = '\0';
+	return moved;
+}
+
+void deleteCharAtIndex(char *str, size_t index) {
+	deleteNCharsAtIndex(str, index, 1);
+}
+
+size_t filterTokens_getTokensAmount(char **tokens) {
+	size_t count = 0;
+	while (tokens[count] != NULL)
+		count++;
+	return count;
+}
+
+size_t filterTokens(char *str, char **tokens, char *dest) {
+	size_t tokensAmount = filterTokens_getTokensAmount(tokens);
+	size_t tokenSizes[tokensAmount];
+	for (size_t i = 0; i < tokensAmount; i++)
+		tokenSizes[i] = strlen(tokens[i]);
+
+	size_t sLen = strlen(str);
+	size_t strIndex = 0;
+	size_t destIndex = 0;
+	while (str[strIndex] != '\0') {
+		for (size_t t = 0; t < tokensAmount; t++) {
+			if (strIndex + tokenSizes[t] <= sLen &&
+			    strncmp(str + strIndex, tokens[t], tokenSizes[t]) == 0) {
+				strIndex += tokenSizes[t];
+				// reset t to check for tokens on the new strIndex position
+				t = -1;
+			}
+		}
+		dest[destIndex++] = str[strIndex++];
+	}
+	dest[destIndex] = '\0';
+	return destIndex - 1;
+}
+
+size_t filterChars(char *str, char *tokens, char *dest) {
+	size_t tokensAmount = strlen(tokens);
+	size_t strIndex = 0;
+	size_t destIndex = 0;
+	while (str[strIndex] != '\0') {
+		for (size_t t = 0; t < tokensAmount; t++) {
+			if (str[strIndex] == tokens[t]) {
+				strIndex++;
+				// reset t to check for tokens on the new strIndex position
+				t = -1;
+			}
+		}
+		dest[destIndex++] = str[strIndex++];
+	}
+	dest[destIndex] = '\0';
+	return destIndex - 1;
+}
+
+char **split(char *str, char delim, size_t *size) {
+	size_t delimAmount = tokencount(str, delim);
+	size_t lastDelimIndex = -1;
+
+	// edge case: first char is delim -> ignore it
+	if (str[0] == delim) {
+		delimAmount--;
+		lastDelimIndex = 0;
+	}
+
+	char **splitArray = ourMalloc((delimAmount + 1) * sizeof(char *));
+	// error checking
+	if (splitArray == NULL) {
+		return NULL;
+	}
+
+	size_t splitIndex = 0;
+	size_t strIndex = 1;
+
+	while (str[strIndex - 1] != '\0') {
+		if (str[strIndex] == delim || str[strIndex] == '\0') {
+			if (splitIndex == delimAmount)
+				break;
+
+			size_t splitSize =
+			    strIndex - lastDelimIndex; // includes the final '\0'
+
+			// ignore the splits with no size
+			if (splitSize <= 1) {
+				lastDelimIndex = strIndex;
+				strIndex++;
+				continue;
+			}
+			splitArray[splitIndex] = ourMalloc(splitSize);
+			// error checking
+			if (splitArray[splitIndex] == NULL) {
+				for (size_t i = 0; i < splitIndex; i++)
+					ourFree(splitArray[splitIndex]);
+				return NULL;
+			}
+
+			strncpy(splitArray[splitIndex], str + lastDelimIndex + 1,
+			        splitSize - 1);
+			splitArray[splitIndex][splitSize - 1] = '\0';
+
+			splitIndex++;
+			lastDelimIndex = strIndex;
+		}
+		strIndex++;
+	}
+	*size = splitIndex;
+	return splitArray;
 }
 
 __attribute__((used, optimize("-fno-tree-loop-distribute-patterns"))) void *
--- Userland/libc/string.h
+++ Userland/libc/string.h
@@ -1,10 +1,54 @@
 #pragma once
 
 #include "stdint.h"
+#include <stddef.h>
 
 uint64_t strlen(char *str);
 void strcpy(char *dst, const char *src);
+void strncpy(char *dst, const char *src, size_t n);
 int strcmp(char *str1, char *str2);
+int strncmp(const char *s1, const char *s2, size_t n);
+size_t tokencount(char *str, char c);
+/**
+ * @brief splits str into an array of strings according to the given delimiter
+ * delim.
+ * If str does not contain delim, returns an array of 1 element with str copied
+ * into it. If the first or last character of str is delim, it ignores it. If
+ * there are two or more delims together, it treats them as one.
+ *
+ * @param str the input string
+ * @param delim the delimiter to split str
+ * @param size the size of the resulting array
+ * @return an array of strings with the splitted input string
+ */
+char **split(char *str, char delim, size_t *size);
+/**
+ * @brief filters from str all appearences of any token in tokens and stores the
+ * result on dest
+ *
+ * @param str input string
+ * @param tokens list of tokens to filter (each as char *) terminated in NULL
+ * @param dest pointer to store the string based on str but without any token
+ * from tokens
+ * @returns size of filtered string (without counting the final '\0')
+ */
+size_t filterTokens(char *str, char **tokens, char *dest);
+
+/**
+ * @brief filters from str all appearences of any charachter in tokens and
+ * stores the result on dest
+ *
+ * @param str input string
+ * @param tokens list of tokens to filter (each char of the string)
+ * @param dest pointer to store the string based on str but without any token
+ * from tokens
+ * @returns size of filtered string (without counting the final '\0')
+ */
+size_t filterChars(char *str, char *tokens, char *dest);
+
+void deleteCharAtIndex(char *str, size_t index);
+// returns the amount of deleted characters
+size_t deleteNCharsAtIndex(char *str, size_t index, size_t n);
 
 void *memset(void *destination, int32_t character, uint64_t length);
 void *memcpy(void *destination, const void *source, uint64_t length);
